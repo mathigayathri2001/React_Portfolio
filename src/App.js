17d267d53d1a858ed636628d9e162337e9b58271
Gayathiri Ramamoorthy <rgayathiri2001@yahoo.com>
2020-07-01 20:33:46 -0400

    added route in the App

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 import React from "react";
-import { BrowserRouter as Router, Route ,Switch} from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/Navbar";
-// import Home from "./pages/Home";
-// import Project from "./pages/Project";
+import Home from "./pages/Home";
+import Project from "./pages/Project";
 
 function App() {
   return (
     <Router>
-
       <div>
-      <Navbar /> 
-{/* <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/About" component={Home} />
-        <Route exact path="/Project" component={Project} />
-        </Switch> */}
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/About" component={Home} />
+          <Route exact path="/Project" component={Project} />
+        </Switch>
       </div>
     </Router>
   );
