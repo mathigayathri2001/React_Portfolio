686df81b3020c2b2b8b9acac063f92ea6ced732c
Gayathiri Ramamoorthy <rgayathiri2001@yahoo.com>
2020-07-02 01:18:46 -0400

    Updates

---
+++ precache-manifest.80dbf9baee9f472203ba09f7fd6a441f.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fedff308a3a94973cf55b9987b0c4de7",
+    "url": "/React_Portfolio/index.html"
+  },
+  {
+    "revision": "1a9d9bbc378fa4d9b368",
+    "url": "/React_Portfolio/static/css/main.abbe18c9.chunk.css"
+  },
+  {
+    "revision": "6e1d1bb4de68029bdfc3",
+    "url": "/React_Portfolio/static/js/2.9fe216b0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React_Portfolio/static/js/2.9fe216b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a9d9bbc378fa4d9b368",
+    "url": "/React_Portfolio/static/js/main.7e351b0c.chunk.js"
+  },
+  {
+    "revision": "3a0598dfa4517aaf1428",
+    "url": "/React_Portfolio/static/js/runtime-main.446e9683.js"
+  },
+  {
+    "revision": "8dd011760a5e4f9be6bfd9fd4791ce76",
+    "url": "/React_Portfolio/static/media/Gayathiri_Ramamoorthy.8dd01176.pdf"
+  },
+  {
+    "revision": "ea9863726b639f7725e6a6360b705986",
+    "url": "/React_Portfolio/static/media/HW1.ea986372.jpg"
+  },
+  {
+    "revision": "411b87abdbd08c80b6da87f16cbca737",
+    "url": "/React_Portfolio/static/media/HW2.411b87ab.jpg"
+  },
+  {
+    "revision": "3a8cbcaf8070cdbd90190956d73205fe",
+    "url": "/React_Portfolio/static/media/about_1.3a8cbcaf.jpg"
+  },
+  {
+    "revision": "21dd68439f347c4551210246341c620f",
+    "url": "/React_Portfolio/static/media/burger.21dd6843.jpg"
+  },
+  {
+    "revision": "484211d5e59639b4ef678d5c29ec217c",
+    "url": "/React_Portfolio/static/media/employee.484211d5.jpg"
+  },
+  {
+    "revision": "a01abfdee3814a98361fbfc651422ad7",
+    "url": "/React_Portfolio/static/media/mealprepbuddy.a01abfde.jpg"
+  },
+  {
+    "revision": "8e56e7fec7420eeb4e091cbac82cf407",
+    "url": "/React_Portfolio/static/media/spamcalltracker.8e56e7fe.jpg"
+  }
+]);
