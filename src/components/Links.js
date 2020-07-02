2e9f664bd2bfe5645e74f25948ef7489943daf37
Gayathiri Ramamoorthy <rgayathiri2001@yahoo.com>
2020-07-01 20:18:36 -0400

    added React icons in package .json and created links component

---
+++ src/components/Links.js
@@ -0,0 +1,35 @@
+import React from "react";
+import "../App.css";
+import {FaLinkedin,FaGithub,FaHome,FaFilePdf } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import resume from "../img/Gayathiri_Ramamoorthy.pdf";
+
+
+function Links() {
+  return (
+    <div >
+     
+      <div className="icon-bar">
+      <h5 id="link">Links</h5>
+       <Link className="active" to="/">
+          <FaHome />
+        </Link> 
+        <a href="https://github.com/mathigayathri2001" target="_blank"rel='noopener noreferrer'>
+          <FaGithub />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/gayathiri-ramamoorthy-72885359/"
+          target="_blank"rel='noopener noreferrer'
+        > <FaLinkedin/>
+         
+        </a>
+        <a id="resume" href= {resume} target="_blank" rel='noopener noreferrer'>
+          <FaFilePdf />
+        </a>
+      </div>
+    </div>
+    
+  );
+}
+
+export default Links;
